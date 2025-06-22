bc238459d4a87d1d9c1f6517348e47c3a66527cf
Abhishek A <abhishek.a2077@gmail.com>
2025-06-22 11:11:24 +0530

    added random password generator

---
+++ random-password-generator/script.js
@@ -0,0 +1,7 @@
+const result=document.getElementById("result")
+const length=document.getElementById("length")
+const btn=document.getElementById("btn")
+const isLetters=document.getElementById("letters")
+const isNumbers=document.getElementById("numbers")
+const isSpecialChars=document.getElementById("specialchars")
+
